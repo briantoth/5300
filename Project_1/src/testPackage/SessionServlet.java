6c5a40a4a5e3b6a0656d42bdc66e890fe565e3b0
Will <wkiffer@gmail.com>
2013-02-25 20:23:59 -0500

    Added a bunch of code we'll all be using

--- Project_1/src/testPackage/SessionServlet.java
+++ Project_1/src/testPackage/SessionServlet.java
@@ -1,9 +1,15 @@
 package testPackage; // Always use packages. Never use default package.
 
-import java.io.*; 
-import javax.servlet.*;
-import javax.servlet.annotation.*;
-import javax.servlet.http.*;
+import java.io.IOException;
+import java.util.Date;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /** Very simplistic servlet that generates plain text.
  *  Uses the @WebServlet annotation that is supported by
@@ -18,42 +24,83 @@ import javax.servlet.http.*;
 public class SessionServlet extends HttpServlet {
   private static long nextSessionID = 0;
   private static final String COOKIE_NAME = "CS5300_WJK56_DRM237_BDT25";
+  private String serverName;
+  private ConcurrentHashMap<String, SessionData> sessionMap;
 	
+  public SessionServlet(String serverName) {
+	  super();
+	  sessionMap = new ConcurrentHashMap<String, SessionServlet.SessionData>();
+	  if(serverName == null)
+		  this.serverName = "Server 1";
+	  else
+		  this.serverName = serverName;
+  }
+  
   @Override
   public void doGet(HttpServletRequest request,
                     HttpServletResponse response)
       throws ServletException, IOException {
 	  
+	  Cookie[] cookies = request.getCookies();
 	  String cmd = request.getParameter("command");
-	  if(cmd == null)
-		  newSessionState(request, response);
-	  else if(cmd == "replace")
-		  replace(request, response);
-	  else if(cmd == "refresh")
-		  refresh(request, response);
-	  else if(cmd == "logout")
-		  logout(request, response); 
-	 
+	  
+	  if(cookies == null) {
+		  Cookie cookie = newSessionState(request, response);
+		  refresh(request, response, cookie);
+	  	  return;
+	  }
+	  else {
+		  for(Cookie cookie : cookies) {
+			  if(COOKIE_NAME.equals(cookie.getName())) {
+				  if(cmd == null || cmd.equals("refresh")) {
+					  refresh(request, response, cookie);
+					  return;
+				  } else if(cmd.equals("replace")) {
+					  replace(request, response, cookie);
+				  	  refresh(request, response, cookie);
+				  	  return;
+				  } else if(cmd.equals("logout")) {
+					  logout(request, response);
+					  return;
+				  }
+			  }
+		  }
+	  }
+	  
+	  //If we got here than there are cookies, but none with
+	  //the name COOKIE_NAME, so we need to create a new session
+	  
+	  Cookie cookie = newSessionState(request, response);
+	  refresh(request, response, cookie);
   }
+				
+		 
 
 private void logout(HttpServletRequest request, HttpServletResponse response) {
 	// TODO Auto-generated method stub
 	
 }
 
-private void newSessionState(HttpServletRequest request,
-		HttpServletResponse response) {
+private Cookie newSessionState(HttpServletRequest request, HttpServletResponse response) {
+	
+	return null;
+	
+}
+
+private void replace(HttpServletRequest request, HttpServletResponse response, Cookie cookie) {
 	// TODO Auto-generated method stub
 	
 }
 
-private void replace(HttpServletRequest request, HttpServletResponse response) {
+private void refresh(HttpServletRequest request, HttpServletResponse response, Cookie cookie) {
 	// TODO Auto-generated method stub
 	
 }
 
-private void refresh(HttpServletRequest request, HttpServletResponse response) {
-	// TODO Auto-generated method stub
-	
+public static class SessionData {
+	public String sessionID;
+	public int version;
+	public String message;
+	public Date expiration_timestamp;
 }
 }
