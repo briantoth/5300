4e1ca285d60307ea0ea6e7bf7554a5763747836a
Brian Toth <bri.toth@gmail.com>
2013-04-29 23:41:10 -0400

    can now actually run mapreduce in a chained fashion on elasticmr

--- Simple_PageRank/src/test/Reduce.java
+++ Simple_PageRank/src/test/Reduce.java
@@ -7,16 +7,15 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-import test.SimplePageRank.CounterGroup;
 
 public class Reduce extends Reducer<LongWritable, Text, LongWritable, Text> {
 	public final double DAMPING_FACTOR = 0.85;
-	
+
 	@Override
-   public void reduce(LongWritable key, Iterable<Text> values, Context context) 
+	public void reduce(LongWritable key, Iterable<Text> values, Context context) 
      throws IOException, InterruptedException {
-       float pr= 0;
-       float old_pr= 0;
+       double pr= 0;
+       double old_pr= 0;
        String receivingNodes = "";
        for (Text val : values) {
     	   StringTokenizer tokenizer = new StringTokenizer(val.toString());
@@ -25,26 +24,30 @@ public class Reduce extends Reducer<LongWritable, Text, LongWritable, Text> {
     	   if (!tokenizer.hasMoreTokens())
     		   return;
     		   
-    	   String nextToken= tokenizer.nextToken();
-    	   if (nextToken.equals("pr")){
+    	   String tok= tokenizer.nextToken();
+    	   if (tok.equals("pr")){
     		   String received_pr= tokenizer.nextToken();
     		   System.out.println("Received pr value: " + received_pr);
-    		   pr+= Float.parseFloat(received_pr);
+    		   pr+= Double.valueOf(received_pr);
     	   } else {
     		   //skip over node number; get page rank
-    		   nextToken= tokenizer.nextToken();
-    		   old_pr= Float.parseFloat(nextToken);
+    		   tok= tokenizer.nextToken();
+    		   old_pr= Double.valueOf(tok);
+    		   System.out.println("Old pr= " + old_pr);
     		   while(tokenizer.hasMoreTokens()){
 	    		   receivingNodes+= " " + tokenizer.nextToken();
     		   }
     	   }
        }
        System.out.println("incoming pr sum: " + pr);
-       System.out.println("Current counter value: " + context.getCounter(CounterGroup.TOTAL_NODES).getValue());
-       pr = (float) (DAMPING_FACTOR * pr + (1-DAMPING_FACTOR) / (float) context.getCounter(CounterGroup.TOTAL_NODES).getValue());
+       org.apache.hadoop.mapreduce.Counter residualSum= context.getCounter(SimplePageRank.CounterGroup.RESIDUAL_SUM);
+       pr = (double) (DAMPING_FACTOR * pr + (1-DAMPING_FACTOR) / (double) SimplePageRank.NUMBER_OF_NODES );
        System.out.println("New pr: " + pr);
-       float residual= Math.abs(old_pr - pr) / pr;
-       context.getCounter(CounterGroup.AVERAGE_RESIDUAL).setValue((long) (context.getCounter(CounterGroup.AVERAGE_RESIDUAL).getValue() + residual * 100000));
-       context.write(key, new Text(key + " " + pr + " " + receivingNodes));
+       double residual= Math.abs(old_pr - pr) / pr;
+       residualSum.setValue((long) ((long) residualSum.getValue() + residual * 100000));
+       System.out.println("key: " + key);
+       Text value= new Text(pr + receivingNodes);
+       System.out.println("value: " + value);
+       context.write(key, value);
    }
 }
